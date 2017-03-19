16446801421f8bcbf75eb5d6ac4ca42835ad5a57
John <pwnerjohn@gmail.com>
2017-03-19 07:03:20 -0400

    start flask-login and sqlalchemy

--- app.py
+++ app.py
@@ -1,13 +1,62 @@
 from flask import Flask, render_template, json, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
-
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 from RoomAPI import RoomHandler, Room
+from werkzeug.security import generate_password_hash, check_password_hash
 
+# setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'whatisthis'
+
 socketio = SocketIO(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 room_handler = RoomHandler()
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rps.db'
+db = SQLAlchemy(app)
+
+
+# flask-login user class
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id',db.Integer , primary_key=True)
+    username = db.Column('username', db.String(20), unique=True , index=True)
+    password = db.Column('password' , db.String(250))
+    email = db.Column('email',db.String(50),unique=True , index=True)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def set_password(self , password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self , password):
+        return check_password_hash(self.password, password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+# routes
 @app.route('/', methods=['GET'])
 def lobby():
     return render_template('lobby.html')
@@ -15,6 +64,34 @@ def lobby():
 @app.route('/rps', methods=['GET'])
 def rps_room():
     return render_template('rps.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = User.query.filter_by(username=username, password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid', 'error')
+        return redirect(url_for('lobby'))
+    login_user(registered_user)
+    flash('Logged in')
+    return redirect(url_for('lobby'))
+
+@app.route('/register', methods=['POST'])
+def register():
+    user = User(request.form['username'], request.form['password'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User registered')
+    return redirect(url_for('lobby'))
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    pass
+
+# flask_login stuff
+
+
 
 # begin chat
 
